d8ca929f3b0f8e820f0efc545c7b5452c34f4310
carlos alvarez <charlie_cha@outlook.com>
2017-06-16 16:38:16 -0500

    yes

--- FaceWebCam.py
+++ FaceWebCam.py
@@ -31,7 +31,7 @@ while cv2.getWindowProperty('Video', 0) >= 0:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
 
-    frame = cv2.imread('faceswap/lol2.jpg')
+    #frame = cv2.imread('faceswap/lol2.jpg')
 
     faces = apply_Haar_filter(frame, haar_faces, 1.3 , 5, 30)
 
@@ -43,12 +43,12 @@ while cv2.getWindowProperty('Video', 0) >= 0:
         for (x2, y2, w2, h2) in eyes:
             cv2.rectangle(frame, (x+x2, y+y2), (x + x2+w2, y + y2+h2), (255,255,0), 2)
 
-        nose = apply_Haar_filter(sub_img, haar_nose, 1.1 , 8, 10)
+        nose = apply_Haar_filter(sub_img, haar_nose, 1.3 , 8, 10)
         for (x2, y2, w2, h2) in nose:
             cv2.rectangle(frame, (x+x2, y+y2), (x + x2+w2, y + y2+h2), (0,0,255), 2) #red
 
         sub_img2 = frame[y + h/2:y+h,x:x+w,:] #only analize half of face for mouth
-        mouth = apply_Haar_filter(sub_img2, haar_mouth, 1.2 , 10, 10)
+        mouth = apply_Haar_filter(sub_img2, haar_mouth, 1.3 , 10, 10)
         for (x2, y2, w2, h2) in mouth:
             cv2.rectangle(frame, (x+x2, y+h/2+y2), (x + x2+w2, y+h/2+y2+h2), (0,255,0), 2) #green
         #cv2.imshow('Face', sub_img)
